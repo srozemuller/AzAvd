07f422547286c75c7dd04350ec11e492a0231462
srozemuller@unica.nl <srozemuller@uniccloud.nl>
2021-07-30 10:02:44 +0200

    file deleted

--- temp.ps1
+++
@@ -1,3 +0,0 @@
-    Set-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent -Name RegistrationToken -Value "asffsadf"
-    Set-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent -Name IsRegistered -Value 0 
-    Restart-Service -Name RDAgentBootLoader
