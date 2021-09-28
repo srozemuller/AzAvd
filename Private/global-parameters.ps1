0209ede66f1ff5dab1e2db84247ea53be2f2efe5
srozemuller@unica.nl <srozemuller@uniccloud.nl>
2021-09-28 17:08:44 +0200

    initial

---
+++ Private/global-parameters.ps1
@@ -0,0 +1,5 @@
+# In this file all global parameters are set. 
+
+$script:AzureApiUrl = "https://management.azure.com"
+$script:GraphApiUrl = "https://graph.microsoft.com"
+$script:GraphApiVersion = "beta"
