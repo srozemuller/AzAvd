3fda9e06e4b362b9f1da0cdd1ffe44c9bbbcf763
srozemuller@unica.nl <srozemuller@uniccloud.nl>
2021-09-29 16:11:24 +0200

    initial correct function

--- Public/Copy-AvdApplicationGroupPermissions.ps1
+++ Public/Copy-AvdApplicationGroupPermissions.ps1
@@ -1,132 +1,59 @@
-function New-AvdHostpool {
+function Copy-AvdApplicationGroupPermissions {
     <#
     .SYNOPSIS
-    Creates a new Azure Virtual Desktop hostpool.
+    Copies application group permissions to another application group
     .DESCRIPTION
-    The function will create a new Azure Virtual Desktop hostpool.
-    .PARAMETER HostpoolName
-    Enter the AVD Hostpool name
-    .PARAMETER ResourceGroupName
-    Enter the AVD Hostpool resourcegroup name
-    .PARAMETER customRdpProperty
-    If needed fill in the custom rdp properties (for example: targetisaadjoined:i:1 )
-    .PARAMETER friendlyName
-    Change the host pool friendly name
-    .PARAMETER loadBalancerType
-    Change the host pool loadBalancerType   
-    .PARAMETER validationEnvironment
-    Change the host pool validation environment   
-    .PARAMETER maxSessionLimit
-    Change the host pool max session limit (max 999999)
-    .PARAMETER Force
-    use the force parameter if you want to override the current customrdpproperties. Otherwise it will add the provided properties.
+    The function will help you copy permissions to another application group. This based on an existing one. 
+    .PARAMETER FromApplicationGroupName
+    Enter the AVD source application group name
+    .PARAMETER FromResourceGroupName
+    Enter the AVD source application group resourcegroup name
+    .PARAMETER ToApplicationGroupName
+    Enter the AVD destination application group name
+    .PARAMETER ToResourceGroupName
+    Enter the AVD destination application group resourcegroup name
     .EXAMPLE
-    New-AvdHostpool -hostpoolname avd-hostpool -resourceGroupName rg-avd-01 -customRdpProperty "targetisaadjoined:i:1"
+    Copy-AvdApplicationGroupPermissions -FromApplicationGroupName avd-appgroup-1 -FromResourceGroupName rg-avd-01 -ToApplicationGroupName avd-appgroup-2 -ToResourceGroupName rg-avd-01 
     #>
     [CmdletBinding()]
-    param
-    (
-        [parameter(Mandatory)]
+    param (
+        [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$HostpoolName,
-    
-        [parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [string]$ResourceGroupName,
-    
-        [parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [string]$location,
+        [string]$FromApplicationGroupName,
 
-        [parameter(Mandatory)]
+        [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [ValidateSet("Pooled", "Personal")]
-        [string]$hostPoolType,
+        [string]$FromResourceGroupName,
 
-        [parameter()]
+        [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$customRdpProperty,
+        [string]$ToApplicationGroupName,
 
-        [parameter()]
+        [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$friendlyName,
-
-        [parameter()]
-        [ValidateSet("BreadthFirst", "DepthFirst")]
-        [ValidateNotNullOrEmpty()]
-        [string]$loadBalancerType,
-        
-        [parameter()]
-        [ValidateNotNullOrEmpty()]
-        [boolean]$validationEnvironment,
-
-        [parameter()]
-        [ValidateNotNullOrEmpty()]
-        [boolean]$startVMOnConnect,
-
-        
-        [parameter()]
-        [ValidateNotNullOrEmpty()]
-        [ValidateSet("Automatic", "Direct")]
-        [string]$PersonalDesktopAssignmentType = "Automatic",
-
-        [parameter()]
-        [ValidateNotNullOrEmpty()]
-        [ValidateRange(1, 999999)]
-        [int]$maxSessionLimit,
-
-        [parameter(ParameterSetName = 'Change')]
-        [switch]$Force
+        [string]$ToResourceGroupName
     )
     Begin {
-        Write-Verbose "Start searching"
+        Write-Verbose "Start searching for hostpool $hostpoolName"
         AuthenticationCheck
-        $token = GetAuthToken -resource "https://management.azure.com"
-        $apiVersion = "?api-version=2019-12-10-preview"
-        $url = "https://management.azure.com/subscriptions/" + $script:subscriptionId + "/resourceGroups/" + $ResourceGroupName + "/providers/Microsoft.DesktopVirtualization/hostpools/" + $HostpoolName + $apiVersion
-        $parameters = @{
-            uri     = $url
-            Headers = $token
-        }
-        $body = @{
-            location = $location
-            properties = @{
-                hostPoolType = $hostPoolType
-            }
-        }
-        if ($customRdpProperty){
-            $getResults = Invoke-RestMethod @parameters
-            $currentCustomRdpProperty = $getResults.properties.customRdpProperty
-        }    
-        if ($friendlyName){$body.properties.Add("friendlyName", $friendlyName)}
-        if ($description){$body.properties.Add("description", $description)}
-        if ($loadBalancerType){$body.properties.Add("loadBalancerType", $loadBalancerType)}
-        if ($validationEnvironment){$body.properties.Add("validationEnvironment", $validationEnvironment)}
-        if ($startVMOnConnect){$body.properties.Add("startVMOnConnect", $startVMOnConnect)}
-        if ($PersonalDesktopAssignmentType){$body.properties.Add("PersonalDesktopAssignmentType", $PersonalDesktopAssignmentType)}
-        if ($maxSessionLimit){$body.properties.Add("maxSessionLimit", $maxSessionLimit)}
     }
     Process {
-         switch ($PsCmdlet.ParameterSetName) {
-            Change {
-                Write-Verbose "Force used, overwriting custom RDP properties."
-                Write-Verbose "Old properties where: $customRdpProperty"
+        $FromApplicationResults = Get-AvdApplicationGroup -ApplicationGroupName $FromApplicationGroupName -ResourceGroupName $FromResourceGroupName
+        $graphToken = GetAuthToken -resource $Script:GraphApiUrl
+        $FromApplicationResults.assignments.properties | ? { $_.Scope -eq "/subscriptions/" + $Script:subscriptionId + "/resourcegroups/" + $FromResourceGroupName + "/providers/Microsoft.DesktopVirtualization/applicationgroups/" + $FromApplicationGroupName }  | ForEach {
+            If ($_.principalType -eq 'User') {
+                $graphUrl = $Script:GraphApiUrl + "/" + $script:GraphApiVersion + "/users/" + $_.principalId
+                $identityInfo = Invoke-RestMethod -Method GET -Uri $graphUrl -Headers $graphToken
+                Write-Verbose "Adding user $($identityInfo.userPrincipalName) to $ToApplicationGroupName"
+                Add-AvdApplicationGroupPermissions -ApplicationGroupName $ToApplicationGroupName -ResourceGroupName $ToResourceGroupName -UserPrincipalName $_.principalId
             }
-            default {
-                If (!($customRdpProperty.EndsWith(";"))) {
-                    $customRdpProperty = $customRdpProperty + ";"
-                }
-                $customRdpProperty = $currentCustomRdpProperty + $customRdpProperty
+            Else {
+                $graphUrl = $Script:GraphApiUrl + "/" + $script:GraphApiVersion + "/groups?`$filter=id eq '$($_.principalId)'"
+                $identityInfo = (Invoke-RestMethod -Method GET -Uri $graphUrl -Headers $graphToken).value
+                Write-Verbose "Adding group $($identityInfo.displayName) to $ToApplicationGroupName"
+                Add-AvdApplicationGroupPermissions -ApplicationGroupName $ToApplicationGroupName -ResourceGroupName $ToResourceGroupName -UserPrincipalName $_.principalId
             }
         }
-        $jsonBody = $body | ConvertTo-Json
-        $parameters = @{
-            uri     = $url
-            Method  = "PUT"
-            Headers = $token
-            Body    = $jsonBody
-        }
-        $results = Invoke-RestMethod @parameters
-        return $results
     }
+    End {}
 }
