64f2de72c45b3e2a89cc4cad18e2a26262f585f6
srozemuller@unica.nl <srozemuller@uniccloud.nl>
2021-10-22 18:28:46 +0200

    new version

--- Public/New-AvdScalingPlan.ps1
+++ Public/New-AvdScalingPlan.ps1
@@ -1,38 +1,66 @@
 function New-AvdScalingPlan {
     <#
     .SYNOPSIS
-    Creates a new Azure Virtual Desktop hostpool.
+    Creates a new Azure Virtual Desktop Scaling plan.
     .DESCRIPTION
-    The function will create a new Azure Virtual Desktop hostpool.
-    .PARAMETER HostpoolName
-    Enter the AVD Hostpool name
+    The function will create a new Azure Virtual Desktop scaling plan and will assign it to (a) hostpool(s).
+    .PARAMETER ScalingPlanName
+    Enter the scaling plan name
     .PARAMETER ResourceGroupName
-    Enter the AVD Hostpool resourcegroup name
-    .PARAMETER customRdpProperty
-    If needed fill in the custom rdp properties (for example: targetisaadjoined:i:1 )
+    Enter the resourcegroup name
+    .PARAMETER Location
+    Enter the location
+    .PARAMETER Description
+    If needed fill in the description
+    .PARAMETER AssignToHostPool
+    Enter the AVD Hostpool names and resource groups (eg. @{"Hostpool-1" = "RG-AVD-01"; "Hostpool-2" = "RG-AVD-02" } -ScheduleDays @("Monday", "WednesDay"))
     .PARAMETER friendlyName
-    Change the host pool friendly name
-    .PARAMETER loadBalancerType
-    Change the host pool loadBalancerType   
-    .PARAMETER validationEnvironment
-    Change the host pool validation environment   
-    .PARAMETER maxSessionLimit
-    Change the host pool max session limit (max 999999)
-    .PARAMETER Force
-    use the force parameter if you want to override the current customrdpproperties. Otherwise it will add the provided properties.
+    Change the scaling plan friendly name
+    .PARAMETER TimeZone
+    Timezone where the plan lives. (default is the timezone where the script is running.)
+    .PARAMETER ScheduleName
+    Enter the schedule name
+    .PARAMETER ScheduleDays
+    Enter the days when the schedule needs to work (eg. @("Monday", "WednesDay"))
+    .PARAMETER rampUpStartTime
+    Enter the start time of the autoscale process
+    .PARAMETER rampUpLoadBalancingAlgorithm
+    How do you like the loadbalancing (DepthFirst, BreadthFirst)
+    .PARAMETER rampUpMinimumHostsPct
+    How many 
+    .PARAMETER rampUpCapacityThresholdPct
+    
+    .PARAMETER peakStartTime
+    Enter the time of the maximum amount of hosts
+    .PARAMETER peakLoadBalancingAlgorithm
+    How do you like the loadbalancing (DepthFirst, BreadthFirst)
+    .PARAMETER rampDownLoadBalancingAlgorithm
+    How do you like the loadbalancing (DepthFirst, BreadthFirst)
+    .PARAMETER rampDownStartTime
+    What time needs the scaling plan shutdown hosts.
+    .PARAMETER rampDownMinimumHostsPct
+    Enter the percentage of hosts which needs to be online.
+    .PARAMETER rampDownCapacityThresholdPct
+    Enter the usage percentage when to start a new host.
+    .PARAMETER rampDownForceLogoffUsers
+    Force logoff users?
+    .PARAMETER rampDownWaitTimeMinutes
+    Number of minutes to wait to stop hosts during ramp down period.
+    .PARAMETER rampDownNotificationMessage
+    Provide the message to send to end users.
+    .PARAMETER offPeakStartTime
+    Whats the scaling plans end time?
+    .PARAMETER offPeakLoadBalancingAlgorithm
+    How do you like the loadbalancing (DepthFirst, BreadthFirst)
     .EXAMPLE
-    New-AvdHostpool -hostpoolname avd-hostpool -resourceGroupName rg-avd-01 -location WestEurope -hostPoolType "Personal"
-    .EXAMPLE
-    New-AvdHostpool -hostpoolname avd-hostpool -resourceGroupName rg-avd-01 -location WestEurope -customRdpProperty "targetisaadjoined:i:1"
-    .EXAMPLE
-    New-AvdHostpool -hostpoolname avd-hostpool -resourceGroupName rg-avd-01 -location WestEurope -vmTemplate "{"domain":"","osDiskType":"Premium_LRS","namePrefix":"avd","vmSize":{"cores":"2","ram":"8","id":"Standard_B2MS"},"galleryImageOffer":"","galleryImagePublisher":"","galleryImageSKU":"","imageType":"","imageUri":"","customImageId":"","useManagedDisks":"True","galleryItemId":null}"
+    New-AvdScalingPlan -ScalingPlanName sp-avd-weekdays -resourceGroupName rg-avd-01 -location WestEurope -AssignToHostpool @{"Hostpool-1" = "RG-AVD-01"; "Hostpool-2" = "RG-AVD-02"} -ScheduleDays @("Monday", "WednesDay")
     #>
     [CmdletBinding()]
     param
     (
         [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$HostpoolName,
+        [string]$ScalingPlanName,
     
         [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
@@ -42,82 +70,155 @@ function New-AvdScalingPlan {
         [ValidateNotNullOrEmpty()]
         [string]$location,
 
+        [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("Pooled")]
+        [string]$HostpoolType = "Pooled",
+
+        [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Description,
+
+        [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$FriendlyName,
+
+        [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [object]$AssignToHostPool,
+
+        [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$TimeZone = (Get-TimeZone).id,
+        
         [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [ValidateSet("Pooled", "Personal")]
-        [string]$hostPoolType,
+        [string]$ScheduleName,
 
-        [parameter()]
+        [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$customRdpProperty,
+        [array]$ScheduleDays,
 
-        [parameter()]
+        [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$friendlyName,
+        [string]$rampUpStartTime,
 
-        [parameter()]
+        [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$description,
+        [ValidateSet("BreadthFirst", "DepthFirst")]
+        [string]$rampUpLoadBalancingAlgorithm,
 
-        [parameter()]
-        [ValidateSet("BreadthFirst", "DepthFirst", "Persistent")]
+        [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$loadBalancerType,
+        [int]$rampUpMinimumHostsPct,
+
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [int]$rampUpCapacityThresholdPct,
+
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$peakStartTime,
+
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("BreadthFirst", "DepthFirst")]
+        [string]$peakLoadBalancingAlgorithm,
         
-        [parameter()]
+        [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [boolean]$validationEnvironment,
+        [string]$rampDownStartTime,
 
-        [parameter()]
+        [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [boolean]$startVMOnConnect,
-         
-        [parameter()]
-        [ValidateNotNullOrEmpty()]
-        [string]$preferredAppGroupType,
+        [ValidateSet("BreadthFirst", "DepthFirst")]
+        [string]$rampDownLoadBalancingAlgorithm,
 
-        [parameter()]
+        [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [ValidateSet("Automatic", "Direct")]
-        [string]$PersonalDesktopAssignmentType = "Automatic",
+        [int]$rampDownMinimumHostsPct,
 
-        [parameter()]
+        [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$vmTemplate,
+        [int]$rampDownCapacityThresholdPct,
 
-        [parameter()]
+        [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [ValidateRange(1, 999999)]
-        [int]$maxSessionLimit,
+        [boolean]$rampDownForceLogoffUsers,
 
-        [parameter(ParameterSetName = 'Change')]
-        [switch]$Force
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [int]$rampDownWaitTimeMinutes,
+
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateLength(1, 250)]
+        [string]$rampDownNotificationMessage,
+
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$offPeakStartTime,
+
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("BreadthFirst", "DepthFirst")]
+        [string]$offPeakLoadBalancingAlgorithm
     )
+    
     Begin {
-        Write-Verbose "Start searching"
+        Write-Verbose "Start creating scaling plan $ScalingPlanName"
         AuthenticationCheck
         $token = GetAuthToken -resource $Script:AzureApiUrl
         $apiVersion = "?api-version=2021-01-14-preview"
-        $scalingPlanName = "Avd-ScalingPlan3"
         $url = $Script:AzureApiUrl + "/subscriptions/" + $script:subscriptionId + "/resourceGroups/" + $ResourceGroupName + "/providers/Microsoft.DesktopVirtualization/scalingPlans/" + $scalingPlanName + $apiVersion
         $body = @{
-            location   = 'westeurope'
+            location   = $Location
             properties = @{
-                description        = "des1"
-                friendlyName       = "friendly"
-                hostPoolType       = "Pooled"
-                timezone           = "W. Europe Standard Time"
-                hostPoolReferences = @(
+                hostPoolType = $HostpoolType
+                timezone     = $TimeZone
+                schedules    = @(
                     @{
-                        hostPoolArmPath    = $hostpool.id
-                        scalingPlanEnabled = $true
+                        name                           = $ScheduleName
+                        daysOfWeek                     = @(
+                            $ScheduleDays
+                        )
+                        rampUpStartTime                = $rampUpStartTime
+                        rampUpLoadBalancingAlgorithm   = $rampUpLoadBalancingAlgorithm
+                        rampUpMinimumHostsPct          = $rampUpMinimumHostsPct
+                        rampUpCapacityThresholdPct     = $rampUpCapacityThresholdPct
+                        peakStartTime                  = $peakStartTime
+                        peakLoadBalancingAlgorithm     = $peakLoadBalancingAlgorithm
+                        rampDownStartTime              = $rampDownStartTime
+                        rampDownLoadBalancingAlgorithm = $rampDownLoadBalancingAlgorithm
+                        rampDownMinimumHostsPct        = $rampDownMinimumHostsPct
+                        rampDownCapacityThresholdPct   = $rampDownCapacityThresholdPct
+                        rampDownForceLogoffUsers       = $rampDownForceLogoffUsers
+                        rampDownWaitTimeMinutes        = $rampDownWaitTimeMinutes
+                        rampDownNotificationMessage    = $rampDownNotificationMessage
+                        offPeakStartTime               = $offPeakStartTime
+                        offPeakLoadBalancingAlgorithm  = $offPeakLoadBalancingAlgorithm
                     }
                 )
             }
         }
+        if ($Description) { $body.properties.Add("Description", $Description) }
+        if ($ExclusionTag) { $body.properties.Add("ExclusionTag", $ExclusionTag) }
+        if ($FriendlyName) { $body.properties.Add("FriendlyName", $FriendlyName) }
+
     }
     Process {
-        $jsonBody = $body | ConvertTo-Json -Depth 4
+        if ($AssignToHostPool) {
+            $hostPoolReferences = New-Object System.Collections.ArrayList
+            $AssignToHostPool.GetEnumerator() | ForEach-Object {
+                $hostpool = Get-AvdHostPool -HostPoolName $_.Key -ResourceGroupName $_.Value
+                $hostPoolReferences.add(@{
+                        hostPoolArmPath    = $hostpool.id
+                        scalingPlanEnabled = $true
+                    })
+            }
+            $body.properties.Add("hostPoolReferences", $hostPoolReferences)
+        }
+        $jsonBody = $body | ConvertTo-Json -Depth 6
         $parameters = @{
             URI     = $url
             Method  = "PUT"
