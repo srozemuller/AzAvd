b57d8d7eef05e998b2611d5e22329cce38769233
srozemuller <srozemuller@gmail.com>
2021-12-16 22:56:07 +0100

    added project url check

--- Az.Avd.tests.ps1
+++ Az.Avd.tests.ps1
@@ -23,6 +23,19 @@ Describe "$module Global module tests" {
             (Join-Path -Path $modulePath -ChildPath "Private") | Should -Exist
         }
         
+        It "$module project URL should reachable" {
+            $content = Get-Content -Path (Join-Path -Path $modulePath -ChildPath "Az.Avd.psd1")
+            $pattern = "ProjectUri ="
+            $url = ($content | Where-Object { $_ -match $pattern }).Replace($pattern, $null).Replace("'", $null)
+            try {
+                Invoke-WebRequest -Uri $Url -UseBasicParsing -DisableKeepAlive -method Head | Out-Null
+                $exits = $true
+            }
+            catch {
+                $exits = $false
+            }
+            $errors.Count | Should -Be $true
+        }
 
         It "$module is valid PowerShell code" {
             $psFile = Get-Content -Path (Join-Path -Path $modulePath -ChildPath "Az.Avd.psm1") -ErrorAction Stop
@@ -38,62 +51,63 @@ Describe "$module Global module tests" {
             $content = Get-Content -Path $file
         }
         It "$($_.Name) should exist" {
-                Test-Path $_ | Should -Be $true
-            }
+            Test-Path $_ | Should -Be $true
+        }
 
-            It "$($_.Name) should have content" {
-                $_.Length | Should -BeGreaterThan 0
-            }
+        It "$($_.Name) should have content" {
+            $_.Length | Should -BeGreaterThan 0
+        }
 
-            It "$($_.Name) should have help block" {
-                $_ | Should -FileContentMatch '<#'
-                $_ | Should -FileContentMatch '#>'
-            }
+        It "$($_.Name) should have help block" {
+            $_ | Should -FileContentMatch '<#'
+            $_ | Should -FileContentMatch '#>'
+        }
  
-            It "$($_.Name) should have a SYNOPSIS section in the help block" {
-                $_ | Should -FileContentMatch '.SYNOPSIS'
-            }
+        It "$($_.Name) should have a SYNOPSIS section in the help block" {
+            $_ | Should -FileContentMatch '.SYNOPSIS'
+        }
     
-            It "$($_.Name) should have a DESCRIPTION section in the help block" {
-                $_ | Should -FileContentMatch '.DESCRIPTION'
-            }
+        It "$($_.Name) should have a DESCRIPTION section in the help block" {
+            $_ | Should -FileContentMatch '.DESCRIPTION'
+        }
  
-            It "$($_.Name) should have a EXAMPLE section in the help block" {
-                $_ | Should -FileContentMatch '.EXAMPLE'
-            }
+        It "$($_.Name) should have a EXAMPLE section in the help block" {
+            $_ | Should -FileContentMatch '.EXAMPLE'
+        }
      
-            It "$($_.Name) should be an advanced function" {
-                $_ | Should -FileContentMatch 'function'
-                $_ | Should -FileContentMatch 'cmdletbinding'
-                $_ | Should -FileContentMatch 'param'
-            }
+        It "$($_.Name) should be an advanced function" {
+            $_ | Should -FileContentMatch 'function'
+            $_ | Should -FileContentMatch 'cmdletbinding'
+            $_ | Should -FileContentMatch 'param'
+        }
        
-            It "$($_.Name) should contain Write-Verbose blocks" {
-                $_ | Should -FileContentMatch 'Write-Verbose'
-            }
+        It "$($_.Name) should contain Write-Verbose blocks" {
+            $_ | Should -FileContentMatch 'Write-Verbose'
+        }
 
-            It "$($_.Name) should not contain Write-Host" {
-                $_ | Should -Not -FileContentMatch 'Write-Host'
-            }
+        It "$($_.Name) should not contain Write-Host" {
+            $_ | Should -Not -FileContentMatch 'Write-Host'
+        }
 
-            It "$($_.Name) should not contain return in functions" {
-                $_ | Should -Not -FileContentMatch 'return `$'
-            }
+        It "$($_.Name) should not contain return in functions" {
+            $_ | Should -Not -FileContentMatch 'return `$'
+        }
 
-            It "$($_.Name) should have an AuthenticationCheck" {
+        It "$($_.Name) should have an AuthenticationCheck" {
                ($content | Select-String -Pattern 'AuthenticationCheck') | Should -Be $true
-            }
+        }
             
-            It "$($_.Name) function start with function name and should be $($_.BaseName) " {
-                $content[0] -match "function $($_.BaseName) {" | Should -Be $true
-            }
+        It "$($_.Name) function start with function name and should be $($_.BaseName) " {
+            $content[0] -match "function $($_.BaseName) {" | Should -Be $true
+        }
 
-            It "$($_.Name) is valid PowerShell code" {
-                $psFile = Get-Content -Path $_ -ErrorAction Stop
-                $errors = $null
-                $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
-                $errors.Count | Should -Be 0
-            }
-        } # foreach ($function in $functions)
+        It "$($_.Name) is valid PowerShell code" {
+            $psFile = Get-Content -Path $_ -ErrorAction Stop
+            $errors = $null
+            $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+            $errors.Count | Should -Be 0
+        }
+
+    } # foreach ($function in $functions)
     
 }
