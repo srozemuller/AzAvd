b14c67fd3307269d57954a9f197de7b3c7760b75
srozemuller@unica.nl <sander@rozemuller.com>
2021-12-17 14:55:34 +0100

    initial

---
+++ Az.Avd.Files.tests.ps1
@@ -0,0 +1,223 @@
+using namespace System.Management.Automation
+using namespace System.Management.Automation.Language
+
+BeforeAll {
+    Function Find-Requirements { 
+        [CmdletBinding(DefaultParameterSetName = 'Path')]
+        param(
+            [Parameter(ValueFromPipeline,
+                ValueFromPipelineByPropertyName,
+                ValueFromRemainingArguments,
+                ParameterSetName = 'Path',
+                Position = 1)]
+            [ValidateNotNull()]
+            [Alias('FilePath')]
+            [string[]]$FullPath = ('.\*.ps1', '.\*.psm1'),
+            [Parameter(Mandatory,
+                ValueFromPipelineByPropertyName,
+                ValueFromRemainingArguments,
+                ParameterSetName = 'Code')]
+            [string[]]$Code,
+            [Parameter(Mandatory,
+                ValueFromPipelineByPropertyName,
+                ValueFromRemainingArguments,
+                ParameterSetName = 'ScriptBlock')]
+            [scriptblock[]]$ScriptBlock,
+            [switch] $unique
+        )
+        switch ($PSCmdlet.ParameterSetName) {
+            'Path' {
+                Write-Verbose -Message 'Path Parameter Set Selected'
+                Write-Verbose "Path contains $FullPath"
+                $ast = [Parser]::ParseFile(
+                    $FullPath,
+                    [ref]$null,
+                    [ref]$null
+                )
+            }
+            'Code' {
+                Write-Verbose -Message 'Code Parameter Set Selected'
+                $ast = foreach ($c in $Code) {
+                    [System.Management.Automation.Language.Parser]::ParseInput($c, [ref]$null, [ref]$null)
+                }  
+            }
+            'ScriptBlock' {
+                Write-Verbose -Message 'ScriptBlock Parameter Set Selected'
+                $ast = $ScriptBlock.ast
+            }
+            'ScriptBlock' {
+                #figure out
+            }
+            default {
+            }
+        }
+        $astCommandSearch = {
+            param (
+                $astItem
+            ) 
+            $astItem -is [Commandast] 
+        }
+        $Commands = $ast.FindAll(
+            $astCommandSearch,
+            $false
+        ) | ForEach-Object { $_.CommandElements[0].value }
+        if ($unique) {
+            $commands | ForEach-Object { (Get-command $_).ModuleName } | Select-Object -Unique  
+        }
+        else {
+            $commands
+        }
+    }
+    $moduleName = "Az.Avd"
+}
+
+$modulePath = Join-Path -Path $(Get-Location) -ChildPath "AzAvd"
+$psFiles = Get-ChildItem -Path (Join-Path -Path $modulePath -ChildPath "Public")
+Describe "Analyze code" -ForEach @(
+    foreach ($file in $psFiles) {
+        @{
+            file        = $file
+            fileName    = $file.Name
+            fileBase    = $file.BaseName
+            content     = Get-Content -Path $file
+            helpInfo    = Get-Help $file.BaseName
+            IgnoreRules = @('PSUseApprovedVerbs')
+            fileobj     = $file 
+        }
+    }
+) {
+    It "<fileName> should exist" {
+        Test-Path $file | Should -Be $true
+    }
+
+    It "<fileName> should have content" {
+        $file.Length | Should -BeGreaterThan 0
+    }
+
+    It "<fileName> should have help block" {
+        $file | Should -FileContentMatch '<#'
+        $file | Should -FileContentMatch '#>'
+    }
+
+    It "<filename> has requirements and no modules mentioned in #requires" {
+        $rawRequirements = Find-Requirements -fullpath $file 
+        $requirements = @()
+        $unfoundCommand = @()
+        $rawRequirements | ForEach-Object { try {
+                $requirements += $_ | Get-command -ErrorAction Stop | Where-Object { $_.source -notlike "microsoft.powershell.*" -and $_ -ne 'Push-OutputBinding' } 
+            }
+            catch {
+                $unfoundCommand += $_
+            }
+        }
+        
+        if ($requirements -and $requiresString.line -notmatch "-modules") { 
+            $requirements | should -be $null -because "Noone likes surprises "
+        }
+        if ($unfoundCommand -and $requiresString.line -notmatch "-modules") { 
+            "<<<<<<<<<<<<`n`nFound undetected commands, please review #requires statement`n$unfoundCommand`n`n>>>>>>>>>>>>>>" | Out-Default
+        }
+    }
+
+    It "<fileName> should have a SYNOPSIS section in the help block" {
+        $file | Should -FileContentMatch '.SYNOPSIS'
+    }
+    It  "<filename> has a synopsis that is valid" { 
+        if ( $helpInfo.synopsis -eq $null) {
+            $helpInfo.synopsis | Should -not -Be $null -Because 'every ps1 file should have a synopsis block'
+        }
+        elseif ( $helpInfo.synopsis -eq 'Short description') {
+            $helpInfo.synopsis | Should -not -Be 'Short description' -Because 'thats just lazy'
+        }
+    }
+    
+    It "<fileName> should have a DESCRIPTION section in the help block" {
+        $file | Should -FileContentMatch '.DESCRIPTION'
+    }
+    It  "<filename> has a description that is valid" { 
+        if ( $helpInfo.description -eq $null) {
+            $helpInfo.description | Should -not -Be $null -Because 'every ps1 file should have a synopsis block'
+        }
+        elseif ( $helpInfo.description -eq 'Long description') {
+            $helpInfo.description | Should -not -Be 'Long description' -Because 'thats just lazy'
+        }
+    }
+    
+    It "<fileName> should have a EXAMPLE section in the help block" {
+        $file | Should -FileContentMatch '.EXAMPLE'
+    }
+    It "<filename> line <linenr> uses the # sign correctly"  -TestCases @(
+        $correctUse = '^#Requires', '^<#', '^#>', '^#region', '^#endregion', '^# ', '##vso\[task.'
+        $comments = (Select-String -Path $file -Pattern '#')
+        ForEach ($comment in $comments) {
+            $correct = ForEach ($use in $correctUse) {
+                ($comment.line.trim() -match $use)
+            }
+            if ($correct -notcontains $true) {
+                $correctComment = $false
+            } 
+            else {
+                $correctComment = $true
+            }
+            @{
+                linenr         = $comment.LineNumber
+                line           = $comment.Line
+                correctUse     = $correctUse
+                correctComment = $correctComment
+            }
+        }
+    ) {
+        $correctComment | should -be $true -because "comment $line should match $($correctUse -join ',' | Out-String)"
+    }
+    
+    It "<fileName> should be an advanced function" {
+        $file | Should -FileContentMatch 'function'
+        $file | Should -FileContentMatch 'cmdletbinding'
+        $file | Should -FileContentMatch 'param'
+    }
+    
+    It "<fileName> should contain Write-Verbose blocks" {
+        $file | Should -FileContentMatch 'Write-Verbose'
+    }
+    
+    It "<fileName> should not contain Write-Host" {
+        $file | Should -Not -FileContentMatch 'Write-Host'
+    }
+    
+    It "<fileName> should not contain return in functions" {
+        $file | Should -Not -FileContentMatch 'return `$'
+    }
+    
+    It "<fileName> should have an AuthenticationCheck" {
+       ($content | Select-String -Pattern 'AuthenticationCheck') | Should -Be $true
+    }
+    
+    It "<fileName> function start with function name and should be $($file.BaseName) " {
+        $content[0] -match "function $($file.BaseName) {" | Should -Be $true
+    }
+    
+    It "<fileBase> function should be available in module" {
+        try {
+            Get-Command $fileBase -Module $moduleName
+        }
+        catch {
+            "$fileBase is not found in commmand list"
+        }
+    }
+    It "Uses PascalCase for Parameter <name>" -TestCases @(
+        foreach ($parameter in $helpInfo.parameters.parameter) {
+            @{
+                name = [string]$parameter.name
+            }
+        }
+    ) {
+        $name | Should -MatchExactly '^[A-Z].*' -Because "PascalCasing" 
+    }
+    
+    It "<fileName> is valid PowerShell code" {
+        $psFile = Get-Content -Path $file -ErrorAction Stop
+        $errors = $null
+        $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+        $errors.Count | Should -Be 0
+    }
+}
---
+++ Az.Avd.Module.tests.ps1
@@ -0,0 +1,60 @@
+$module = 'Az.Avd'
+Describe "$module Global module tests" {
+    Context 'Module Setup' {
+        BeforeAll {
+            $modulePath = Join-Path -Path $(Get-Location) -ChildPath "AzAvd"
+            $binaryFile = "Az.Avd.psm1"
+            $manifestFile = "Az.Avd.psd1"
+            $moduleContent = Get-Content -Path (Join-Path -Path $modulePath -ChildPath $manifestFile)
+        }
+        It "$module has the root module $module.psm1" {
+            $modulePath | Should -Exist
+        }
+ 
+        It "$module has the a manifest file of $module.psm1" {
+            (Join-Path -Path $modulePath -ChildPath $binaryFile) | Should -Exist
+            (Join-Path -Path $modulePath -ChildPath $manifestFile) | Should -Exist
+        }
+
+        It "$module folder has functions folder" {
+            (Join-Path -Path $modulePath -ChildPath "Public") | Should -Exist
+        }
+ 
+        It "$module folder has private functions folder" {
+            (Join-Path -Path $modulePath -ChildPath "Private") | Should -Exist
+        }
+        
+        It "$module root module should be $binaryFile in $manifestFile" {
+            $pattern = "RootModule"
+            $rootModule = ($moduleContent | Where-Object { $_ -match $pattern }).Split("'")[1]
+            $rootModule | Should -Be $binaryFile
+        } 
+        
+        It "$module project URL should reachable" {
+            $pattern = "ProjectUri"
+            $url = ($moduleContent | Where-Object { $_ -match $pattern }).Split("'")[1]
+            try {
+                Invoke-WebRequest -Uri $Url -UseBasicParsing -DisableKeepAlive -method Head | Out-Null
+                $exists = $true
+            }
+            catch {
+                $exists = $false
+            }
+            $exists | Should -Be $true
+        }
+
+        It "$module version should be greater than PSGallery" {
+            $pattern = "ModuleVersion"
+            $sourceVersion = ($moduleContent | Where-Object { $_ -match $pattern }).Split("'")[1]
+            $galleryVersion = (Find-Module -Name Az.Avd -Repository PSGallery).Version
+            [version]$sourceVersion | Should -BeGreaterThan ([version]$galleryVersion)
+        }      
+
+        It "$module is valid PowerShell code" {
+            $psFile = Get-Content -Path (Join-Path -Path $modulePath -ChildPath $binaryFile) -ErrorAction Stop
+            $errors = $null
+            $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+            $errors.Count | Should -Be 0
+        }
+    }
+}
