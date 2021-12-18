3fcfe49940193826af942fc18d022d16a30921f7
srozemuller <srozemuller@gmail.com>
2021-12-18 09:21:18 +0100

    folder changed

--- Compiler/Az.Avd.Files.tests.ps1
+++ Compiler/Az.Avd.Files.tests.ps1
@@ -1,4 +1,4 @@
-$modulePath = Join-Path -Path ".././" -ChildPath "Az.Avd"
+$modulePath = Join-Path -Path (Join-Path ".././" -ChildPath "AzAvd") -ChildPath "Az.Avd"
 $psFiles = Get-ChildItem -Path (Join-Path -Path $modulePath -ChildPath "Public")
 
 Describe "Analyze code" -ForEach @(
