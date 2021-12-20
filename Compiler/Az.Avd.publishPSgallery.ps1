87d4186657f1c81d89ce059a9747aada23537c4e
Sander Rozemuller <srozemuller@gmail.com>
2021-12-20 15:42:53 +0100

    to path

--- Compiler/Az.Avd.publishPSgallery.ps1
+++ Compiler/Az.Avd.publishPSgallery.ps1
@@ -7,8 +7,7 @@ $env:ProjectName = "Az.Avd"
 if ($env:GITHUB_REF_NAME -eq 'main') {
     try {
         $modulePath = Join-Path -Path (Join-Path ".././AzAvd" -ChildPath $env:ProjectName) -ChildPath "Az.Avd.psd1"     
-        Import-Module $modulePath -Force
-        Publish-Module -Name $env:ProjectName -NuGetApiKey $PS_GALLERY_KEY -RequiredVersion (Import-PowerShellDataFile $modulePath).ModuleVersion
+        Publish-Module -Path (Join-Path ".././AzAvd" -ChildPath $env:ProjectName) -NuGetApiKey $PS_GALLERY_KEY -RequiredVersion (Import-PowerShellDataFile $modulePath).ModuleVersion
         write-host "Module $env:ProjectName published"
     }   
     catch {
