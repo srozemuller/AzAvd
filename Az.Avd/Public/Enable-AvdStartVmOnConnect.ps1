9b852d6f9b71897601addbea4da68029f26a36b2
srozemuller <srozemuller@gmail.com>
2021-12-22 20:25:22 +0100

    errorhandling added

--- Az.Avd/Public/Enable-AvdStartVmOnConnect.ps1
+++ Az.Avd/Public/Enable-AvdStartVmOnConnect.ps1
@@ -39,6 +39,9 @@ function Enable-AvdStartVmOnConnect {
         [ValidateNotNullOrEmpty()]
         [string]$RoleName,
 
+        [parameter(ParameterSetName = 'Initial')]
+        [parameter(ParameterSetName = 'Update')]
+        [ValidateNotNullOrEmpty()]
         [switch]$Force
     )
     Begin {
@@ -64,6 +67,7 @@ function Enable-AvdStartVmOnConnect {
                 $HostsResourceGroup = ((Get-AvdLatestSessionHost @parameters).Id -split "/")[4]
             }
         }
+
         $Hostpool = Get-AvdHostPool @parameters
         if ($Force) {
             Update-AvdHostPool @parameters -ValidationEnvironment:$true
@@ -74,51 +78,60 @@ function Enable-AvdStartVmOnConnect {
         }    
         #Region get Windows Virtual Desktop Service Principal
 
-
-        #Region create custom role
-        # Building a new role GUID
-        $Scope = "/subscriptions/"+ $script:subscriptionId +"/Resourcegroups/$HostsResourceGroup"
-        $RoleGuid = (New-Guid).Guid
-        # Generating the role body
-        $RoleBody = @{
-            name       = $RoleGuid
-            properties = @{
-                roleName         = $roleName
-                description      = "This role is used to start VM when connecting"
-                assignableScopes = @(
-                    $Scope
-                )
-                permissions      = @(
-                    @{
-                        actions        = @(
-                            "Microsoft.Compute/virtualMachines/start/action",
-                            "Microsoft.Compute/virtualMachines/read"
-                        )
-                        notActions     = @()
-                        dataActions    = @()
-                        notDataActions = @()
-                    }
-                )
-            }
-        }
-        $RoleJsonBody = $RoleBody | ConvertTo-Json -Depth 5
-        $DefinitionUrl = $Script:AzureApiUrl + $Scope + "/providers/Microsoft.Authorization/roleDefinitions/" +$RoleGuid + "?api-version=2018-07-01"
-        $CustomRole = Invoke-RestMethod -Method PUT -Body $RoleJsonBody -Headers $AzureHeader -URi $DefinitionUrl
-        #Endregion
-
-        #region create assignment
-        # New assignment GUID
-        $ServicePrincipals.value.id | ForEach-Object {
-            $AssignGuid = (New-Guid).Guid
-            $AssignURL = $Script:AzureApiUrl + $Scope + "/providers/Microsoft.Authorization/roleAssignments/" + $AssignGuid + "?api-version=2021-04-01-preview"
-            $assignBody = @{
+        try {
+            #Region create custom role
+            # Building a new role GUID
+            $Scope = "/subscriptions/" + $script:subscriptionId + "/Resourcegroups/$HostsResourceGroup"
+            $RoleGuid = (New-Guid).Guid
+            # Generating the role body
+            $RoleBody = @{
+                name       = $RoleGuid
                 properties = @{
-                    roleDefinitionId = $CustomRole.id
-                    principalId      = $_
+                    roleName         = $roleName
+                    description      = "This role is used to start VM when connecting"
+                    assignableScopes = @(
+                        $Scope
+                    )
+                    permissions      = @(
+                        @{
+                            actions        = @(
+                                "Microsoft.Compute/virtualMachines/start/action",
+                                "Microsoft.Compute/virtualMachines/read"
+                            )
+                            notActions     = @()
+                            dataActions    = @()
+                            notDataActions = @()
+                        }
+                    )
                 }
             }
-            $JsonBody = $assignBody | ConvertTo-Json 
-            Invoke-RestMethod -Method PUT -Uri $AssignURL -Headers $AzureHeader -Body $JsonBody
+    
+            $RoleJsonBody = $RoleBody | ConvertTo-Json -Depth 5
+            $DefinitionUrl = $Script:AzureApiUrl + $Scope + "/providers/Microsoft.Authorization/roleDefinitions/" + $RoleGuid + "?api-version=2018-07-01"
+            $CustomRole = Invoke-RestMethod -Method PUT -Body $RoleJsonBody -Headers $AzureHeader -URi $DefinitionUrl
+            #Endregion
+        }
+        catch {
+            Throw "Role $roleName not created, $_"
+        }
+        try {
+            #region create assignment
+            # New assignment GUID
+            $ServicePrincipals.value.id | ForEach-Object {
+                $AssignGuid = (New-Guid).Guid
+                $AssignURL = $Script:AzureApiUrl + $Scope + "/providers/Microsoft.Authorization/roleAssignments/" + $AssignGuid + "?api-version=2021-04-01-preview"
+                $assignBody = @{
+                    properties = @{
+                        roleDefinitionId = $CustomRole.id
+                        principalId      = $_
+                    }
+                }
+                $JsonBody = $assignBody | ConvertTo-Json 
+                Invoke-RestMethod -Method PUT -Uri $AssignURL -Headers $AzureHeader -Body $JsonBody
+            }
+        }
+        catch {
+            Throw "Assigning $roleName not succesful, $_"
         }
     }
 }
