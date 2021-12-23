cb424f80c965d3bc719a1e3f73acf2d362fffad6
Sander Rozemuller <srozemuller@gmail.com>
2021-12-23 10:33:20 +0100

    add new parameter

--- Az.Avd/Private/global-parameters.ps1
+++ Az.Avd/Private/global-parameters.ps1
@@ -3,3 +3,4 @@
 $script:AzureApiUrl = "https://management.azure.com"
 $script:GraphApiUrl = "https://graph.microsoft.com"
 $script:GraphApiVersion = "beta"
+$script:AvdModuleLocation = "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_11-22-2021.zip"
