43cdf2b549189563875f86157940a3e22770e525
Sander Rozemuller <srozemuller@uniccloud.nl>
2022-03-18 14:46:45 +0100

    last test added

--- Az.Avd/Public/New-AvdVmTemplate.ps1
+++ Az.Avd/Public/New-AvdVmTemplate.ps1
@@ -160,13 +160,22 @@ function New-AvdVmTemplate {
                 Headers = $token
                 Body    = $jsonBody
             }
-            Write-Information "VM template added to hostpool $HostpoolName"
+            Write-Information "VM template added to hostpool $HostpoolName" -InformationAction Continue
             Write-Verbose "Template added with following values (in JSON format):"
             Write-Verbose $vmTemplate
             Invoke-RestMethod @parameters
         }
         catch {
             "Template not added, $_"
+        }
+    }
+    End {
+        try {
+            Write-Information "Testing for template in hostpool $HostpoolName" -InformationAction Continue
+            (Invoke-RestMethod @parameters).properties.vmTemplate | ConvertFrom-Json
+        }
+        catch {
+            Throw "No template found! $_"
         }
     }
 }
