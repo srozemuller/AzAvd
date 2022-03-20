7a6213b4a92d7601c6226f591e4951d571aa66c4
Sander Rozemuller <srozemuller@uniccloud.nl>
2022-03-20 09:47:44 +0100

    code optimize

--- Compiler/Az.Avd.generateRelease.ps1
+++ Compiler/Az.Avd.generateRelease.ps1
@@ -11,49 +11,9 @@ param (
     [Parameter(Mandatory)]
     [boolean]$PreRelease
 )
-try {
-    $githubUrl = "$env:GITHUB_API_URL/repos/$env:GITHUB_REPOSITORY/releases?access_token=$GitHubKey"
-    $getReleaseParams = @{
-        Uri    = $githubUrl
-        Method = 'GET'
-        Header = @{
-            Accept        = 'application/vnd.github.v3+json'
-            Authorization = "token $GitHubKey"
-        }
-    }
-    $releases = Invoke-RestMethod @getReleaseParams
-}
-catch {
-    Throw "Not able to find releases"
-}
-try {
-    $env:ProjectName = "Az.Avd"
-    Write-Host "Creating GitHub release" -ForegroundColor Green
-    $modulePath = "./$env:ProjectName/$env:ProjectName.psd1"
-    $manifest = Import-PowerShellDataFile -Path $modulePath
-    Import-Module $modulePath -Force
-}
-catch {
-    Throw "Not able to import $env:ProjectName and determine current version"
-}
-switch ($BranchName) {
-    beta {
-        Write-Information "Found $($releases[0])" -InformationAction Continue
-        $betaNumberLocation = $releases[0].tag_name.lastindexOf(".")
-        $newNumber = 0;
-        if ($releases[0].tag_name -match $BranchName){
-            $newNumber = [int]$releases[0].tag_name.substring($betaNumberLocation + 1) + 1
-        }
-        $releaseName = 'v{0}-beta.{1}' -f $manifest.ModuleVersion, $newNumber
-    }
-    #default is main branch
-    main {
-        $releaseName = 'v{0}-stable' -f $manifest.ModuleVersion 
-    }
-    default {
-        $releaseName = 'v{0}-{1}' -f $manifest.ModuleVersion, $BranchName
-    }  
-}
+$env:ProjectName = "AzAvd"
+$env:zipLocation = "./{0}_{1}.zip" -f $env:ProjectName, $TagName
+
 try {
     #Publish-Module -Name $env:ProjectName -NuGetApiKey $env:PS_GALLERY_KEY
     $releaseData = @{
@@ -82,14 +42,13 @@ catch {
     Throw "Not able to create a release, $_"
 }
 try {
-    Compress-Archive -DestinationPath "./$($env:ProjectName)_$($manifest.ModuleVersion).zip" -Path "./$env:ProjectName/Az.Avd*"
+    Compress-Archive -DestinationPath $env:zipLocation -Path "./$env:ProjectName/Az.Avd*"
 
     $uploadParams = @{
-        Uri         = ($newRelease.upload_url -replace '\{\?name.*\}', '?name=AzAvd_') +
-        $manifest.ModuleVersion + '.zip'
+        Uri         = ($newRelease.upload_url -replace '\{\?name.*\}', '?name=AzAvd_') + $TagName + '.zip'
         Method      = 'POST'
         ContentType = 'application/zip'
-        InFile      = "./$($env:ProjectName)_$($manifest.ModuleVersion).zip"
+        InFile      =  $env:zipLocation
         Header      = @{
             Authorization = "token $GitHubKey"
         }
