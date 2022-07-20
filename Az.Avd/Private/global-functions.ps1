ddfa273feadb71213901f8e2dcba7a5a7a09ae35
Sander Rozemuller <srozemuller@uniccloud.nl>
2022-07-20 10:11:52 +0200

    init

--- Az.Avd/Private/global-functions.ps1
+++ Az.Avd/Private/global-functions.ps1
@@ -161,3 +161,12 @@ function ConcatSessionHostName {
     }
     $name
 }
+
+function TestAzResource($resourceId) {
+    $testParameters = @{
+        method = "GET"
+        headers = GetAuthToken -resource $script:AzureApiUrl
+        uri = "{0}{1}?api-version=2021-04-01" -f $script:AzureApiUrl, $resourceId
+    }
+    Invoke-RestMethod @testParameters
+}
