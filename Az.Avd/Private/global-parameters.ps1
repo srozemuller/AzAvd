f160aa3055c42bb52d3a72f91aab68e015f2f06e
Sander Rozemuller <srozemuller@uniccloud.nl>
2022-07-22 08:46:12 +0200

    trusted launch added

--- Az.Avd/Private/global-parameters.ps1
+++ Az.Avd/Private/global-parameters.ps1
@@ -3,4 +3,4 @@
 $script:AzureApiUrl = "https://management.azure.com"
 $script:GraphApiUrl = "https://graph.microsoft.com"
 $script:GraphApiVersion = "beta"
-$script:AvdModuleLocation = "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_11-22-2021.zip"
+$script:AvdModuleLocation = "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_06-15-2022.zip"
