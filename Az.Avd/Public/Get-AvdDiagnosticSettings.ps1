e488e57d35c5efea5b0129dafc49bc592d2b3958
Sander Rozemuller <srozemuller@uniccloud.nl>
2022-10-29 19:29:37 +0200

    name set updated

--- Az.Avd/Public/Get-AvdDiagnosticSettings.ps1
+++ Az.Avd/Public/Get-AvdDiagnosticSettings.ps1
@@ -1,19 +1,19 @@
 function Get-AvdDiagnosticSettings {
     <#
     .SYNOPSIS
-    Updates the AVD Diagnostics settings to an another LogAnalytics workspace or categories. 
+    Gets the AVD Diagnostics settings to an another LogAnalytics workspace or categories. 
     .DESCRIPTION
     This command will help you updating the Log Analytics workspace or adding/removing log catagories.
     .PARAMETER HostPoolName
     Enter the name of the hostpool you want to enable start vm on connnect.
     .PARAMETER ResourceGroupName
     Enter the name of the resourcegroup where the hostpool resides in.
-    .PARAMETER Workspace
-    Enter the name(s) of the AVD workspaces
-    .PARAMETER LAWorkspace
-    Enter the name of the Log Analytics Workspace
+    .PARAMETER HostpoolId
+    Enter the hostpool's resource ID
     .EXAMPLE
-    Update-AvdDiagnostics -HostPoolName avd-hostpool-001 -ResourceGroupName rg-avd-001 -AvdWorkspace avd-workpace-001 -DiagnosticsName 
+    Get-AvdDiagnostics -HostPoolName avd-hostpool-001 -ResourceGroupName rg-avd-001
+    .EXAMPLE
+    Get-AvdDiagnostics -HostPoolId "/subscriptions/...."
     #>
     [CmdletBinding(DefaultParameterSetName = 'Name')]
     param (
@@ -39,6 +39,7 @@ function Get-AvdDiagnosticSettings {
     Process {
         switch ($PsCmdlet.ParameterSetName) {
             Name {
+                $HostpoolId = (Get-AvdHostPool @parameters).id
                 $uri =  "{0}/{1}/providers/microsoft.insights/diagnosticSettings/?api-version=2017-05-01-preview" -f $Script:AzureApiUrl, $HostpoolId
             }
             Id {
