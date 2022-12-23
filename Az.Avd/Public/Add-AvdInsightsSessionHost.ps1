dde58d4ff2c703c4598eab291d0176aeaf596a57
Sander Rozemuller <srozemuller@uniccloud.nl>
2022-12-23 10:46:36 +0100

    to v1.0

--- Az.Avd/Public/Add-AvdInsightsSessionHost.ps1
+++ Az.Avd/Public/Add-AvdInsightsSessionHost.ps1
@@ -1,35 +1,26 @@
-function Enable-AvdInsightsCounters {
+function Add-AvdInsightsSessionHost {
     <#
     .SYNOPSIS
-    Enables the AVD Diagnostics and will send it to a new LogAnalytics workspace
+    Adds an AVD session host to the AVD Insights workbook.
     .DESCRIPTION
-    The function will enable AVD diagnostics for a hostpool. It will create a new Log Analytics workspace if no existing workspace is provided.
-    .PARAMETER WorkspaceName
-    Enter the name of the hostpool you want to enable start vm on connnect.
-    .PARAMETER ResourceGroupName
-    Enter the name of the resourcegroup where the hostpool resides in.
+    The function will install the needed extensions on the AVD session host.
     .PARAMETER Id
-    Enter the host pool's resource ID.
+    Enter the session host's resource ID (Not VM, use Get-AvdSessionHost or Get-AvdSessionHostResources to get the ID).
+    .PARAMETER WorkSpaceId
+    Enter the Log Analytics Workspace's resource ID.
     .PARAMETER LASku
     Enter the name of the Log Analytics SKU
     .PARAMETER LAWorkspace
     Enter the name of the Log Analytics Workspace
     .PARAMETER LaResourceGroupName
     Enter the name of the Log Analyics Workspace resource group
-    .PARAMETER AdditionalCategories
-    The categories you like extra to save in Log Analytics, beside the mandatory categories for AVD Insights.
-    .PARAMETER RetentionInDays
-    How long should the data be saved
-    .PARAMETER AutoCreate
-    Use this switch to auto create a Log Analtyics Workspace
     .EXAMPLE
-    Enable-AvdHostpoolInsights -HostPoolName avd-hostpool-001 -ResourceGroupName rg-avd-001 -LAWorkspace 'la-avd-workspace' -Categories ("Checkpoint","Error")
+    Add-AvdInsightsSessionHost -Id /subscriptions/../sessionhosts/avd-0 -WorkSpaceId /subscriptions/../Microsoft.OperationalInsights/workspaces/laworkspace
     .EXAMPLE
-    Enable-AvdHostpoolInsights -Id /subscription/.../ -LAWorkspace 'la-avd-workspace' -Categories ("Checkpoint","Error") -LaResourceGroupName 'la-rg' -LaLocation 'westeurope' -RetentionInDays 30 -AutoCreate
+    Add-AvdInsightsSessionHost -Id /subscriptions/../sessionhosts/avd-0 -LAWorkspace laworkspace -LaResourceGroupName rg-la-01
     #>
     [CmdletBinding(DefaultParameterSetName = 'Id')]
     param (
-
         [parameter(Mandatory, ValueFromPipelineByPropertyName)]
         [ValidateNotNullOrEmpty()]
         [string]$Id,
@@ -39,26 +30,10 @@ function Enable-AvdInsightsCounters {
         [string]$WorkSpaceId,
 
         [parameter(Mandatory, ParameterSetName = 'WorkspaceName')]
-        [parameter(Mandatory, ParameterSetName = 'Create-Friendly')]
         [string]$LAWorkspace,
 
-        [parameter(ParameterSetName = 'Create-Friendly')]
-        [ValidateSet("CapacityReservation", "Free", "LACluster", "PerGB2018", "PerNode", "Premium", "Standalone", "Standard")]
-        [string]$LASku = "Standard",
-
         [parameter(Mandatory, ParameterSetName = 'WorkspaceName')]
-        [parameter(Mandatory, ParameterSetName = 'Create-Friendly')]
-        [string]$LaResourceGroupName,
-        
-        [parameter(Mandatory, ParameterSetName = 'Create-Friendly')]
-        [string]$LaLocation,
-
-        [parameter(Mandatory, ParameterSetName = 'Create-Friendly')]
-        [int]$RetentionInDays,
-
-        [parameter(Mandatory, ParameterSetName = 'Create-Friendly')]
-        [switch]$AutoCreate
-        
+        [string]$LaResourceGroupName
     )
     Begin {
         AuthenticationCheck
@@ -70,11 +45,11 @@ function Enable-AvdInsightsCounters {
                 $WorkSpaceId = "/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.OperationalInsights/workspaces/{2}" -f $script:subscriptionId, $LaResourceGroupName, $LAWorkspace
             }
             default {
-                Write-Verbose "Got a session host's resource ID. Thank you for that!"
+                Write-Verbose "[Add-AvdInsightsSessionHost] - Got a session host's resource ID. Thank you for that!"
             }
         }
-        Write-Verbose "Looking for workspace"
-        Write-Verbose $laWorkspaceId
+        Write-Verbose "[Add-AvdInsightsSessionHost] - Looking for workspace"
+        Write-Verbose $WorkSpaceId
         $laws = Get-Resource -Method "GET" -ResourceId $WorkSpaceId -ApiVersion $script:diagnosticsApiVersion -Verbose
         $lawsKey = Get-Resource -Method "POST" -ResourceId $WorkSpaceId -ApiVersion $script:diagnosticsApiVersion -UrlAddition '/sharedKeys'  -Verbose 
         
@@ -83,35 +58,54 @@ function Enable-AvdInsightsCounters {
         }
         else {
             try {
-                Write-Information "Workspace and sessionhosts found, adding to AVD Insights" -InformationAction Continue
+                Write-Information "[Add-AvdInsightsSessionHost] -  Workspace and sessionhosts found, adding to AVD Insights" -InformationAction Continue
                 $sessionhostResource = Get-AvdSessionHostResources -Id $Id
-                $sessionhostResource.vmResources | ForEach-Object {
+                $sessionhostResource | ForEach-Object {
+                    $vmObject = $_
+                    $vmPowerState = Get-AvdSessionHostPowerState -Id $vmObject.id
+                    if ($vmPowerState.powerstate -ne 'running') {
+                        Write-Warning "[Add-AvdInsightsSessionHost] - Sessionhost $($_.name) not running, starting machine from $($vmPowerState.powerstate) state"
+                        Start-AvdSessionHost -Id $vmObject.id
+                    }
                     $extensionBody = @{
-                        location = $_.location
+                        location   = $_.vmResources.location
                         properties = @{
-                            publisher = "Microsoft.EnterpriseCloud.Monitoring"
-                            type = "MicrosoftMonitoringAgent"
-                            typeHandlerVersion = "1.0"
+                            publisher               = "Microsoft.EnterpriseCloud.Monitoring"
+                            type                    = "MicrosoftMonitoringAgent"
+                            typeHandlerVersion      = "1.0"
                             autoUpgradeMinorVersion = $true
-                            settings = @{
+                            settings                = @{
                                 workspaceId = $laws.properties.customerId
                             }
-                            protectedSettings = @{
+                            protectedSettings       = @{
                                 workspaceKey = $lawsKey.primarySharedKey
                             }
                         }
                     }
                     $requestParameters = @{
-                        uri     = "{0}{1}/extensions/{2}?api-version={3}" -f $Script:AzureApiUrl, $_.id, "OMSExtenstion", "2022-08-01"
+                        uri     = "{0}{1}/extensions/{2}?api-version={3}" -f $Script:AzureApiUrl, $_.vmResources.id, "OMSExtenstion", "2022-08-01"
                         Method  = "PUT"
                         Headers = $token
                         Body    = $extensionBody | ConvertTo-Json -Depth 99
                     }
                     Invoke-RestMethod @requestParameters
+                    switch ($vmPowerState.powerstate){
+                        stopped {
+                            Write-Information "[Add-AvdInsightsSessionHost] - Sessionhost was $($vmPowerState.powerstate), bringing back to initial state" -InformationAction Continue
+                            Stop-AvdSessionHost -Id $vmObject.id
+                        }
+                        deallocated {
+                            Write-Information "[Add-AvdInsightsSessionHost] - Sessionhost was $($vmPowerState.powerstate), bringing back to initial state" -InformationAction Continue
+                            Stop-AvdSessionHost -Id $vmObject.id -Deallocate
+                        }
+                        default {
+                            Write-Information "[Add-AvdInsightsSessionHost] - Sessionhost was $($vmPowerState.powerstate), taking no further action" -InformationAction Continue
+                        }
+                    }
                 }
             }
             catch {
-                Throw $_
+                Throw "[Add-AvdInsightsSessionHost] - $_"
             }
         }
     }
