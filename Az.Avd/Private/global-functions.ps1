9d2f5fc0a620f30e5a3785f2cefb7b5f3d90a30e
Sander Rozemuller <srozemuller@uniccloud.nl>
2023-01-23 13:08:15 +0100

    apiversion added

--- Az.Avd/Private/global-functions.ps1
+++ Az.Avd/Private/global-functions.ps1
@@ -46,11 +46,11 @@ function Create-CategoryArray ($Categories) {
     return  $categoryArray    
 }
 
-function TestAzResource($resourceId) {
+function TestAzResource($resourceId,$apiVersion) {
     $testParameters = @{
         method = "GET"
         headers = GetAuthToken -resource $script:AzureApiUrl
-        uri = "{0}{1}?api-version=2021-04-01" -f $script:AzureApiUrl, $resourceId
+        uri = "{0}{1}?api-version={2}" -f $script:AzureApiUrl, $resourceId, $apiVersion
     }
     Invoke-RestMethod @testParameters
 }
