4f731eed2a2300cdf319d64d1d5cccc9c429769d
Sander Rozemuller <s.rozemuller@exitecloud.com>
2023-03-10 16:00:45 +0100

    unassign all option added

--- Az.Avd/Public/Update-AvdSessionHost.ps1
+++ Az.Avd/Public/Update-AvdSessionHost.ps1
@@ -16,10 +16,12 @@ function Update-AvdSessionHost {
     Enter a friendly name for the current sessionhost. 
     .PARAMETER SessionHostName
     Enter the sessionhosts name avd-hostpool/avd-host-1.avd.domain
+    .PARAMETER UnassignAll
+    Unassign all users from the sessionhost.
     .EXAMPLE
     Update-AvdSessionHost -HostpoolName avd-hostpool -ResourceGroupName rg-avd-01 -SessionHostName avd-hostpool/avd-host-1.avd.domain -AllowNewSession $true
     .EXAMPLE
-    Update-AvdSessionHost -HostpoolName avd-hostpool -ResourceGroupName rg-avd-01 -SessionHostName avd-hostpool/avd-host-1.avd.domain -AssignedUser "" -Force
+    Update-AvdSessionHost -HostpoolName avd-hostpool -ResourceGroupName rg-avd-01 -SessionHostName avd-hostpool/avd-host-1.avd.domain -AssignedUser "user@domain.com" -Force
     #>
     [CmdletBinding(DefaultParameterSetName = 'Id')]
     param
@@ -45,17 +47,26 @@ function Update-AvdSessionHost {
 
         [parameter(ParameterSetName = 'Hostname')]
         [parameter(ParameterSetName = 'Id')]
+        [parameter(ParameterSetName = 'Id-All')]
         [ValidateNotNullOrEmpty()]
         [string]$FriendlyName,
 
         [parameter(Mandatory, ParameterSetName = 'Hostname')]
+        [parameter(Mandatory, ParameterSetName = 'Hostname-All')]
         [string]$SessionHostName,
 
         [parameter(Mandatory, ParameterSetName = 'Id')]
+        [parameter(Mandatory, ParameterSetName = 'Id-All')]
         [object]$Id,
 
         [parameter()]
-        [switch]$Force
+        [switch]$Force,
+
+        [parameter(ParameterSetName = 'All')]
+        [parameter(ParameterSetName = 'Hostname')]
+        [parameter(ParameterSetName = 'Id-All')]
+        [parameter(ParameterSetName = 'Hostname-All')]
+        [switch]$UnAssignAll
     )
     Begin {
         Write-Verbose "Start updating session hosts"
@@ -68,27 +79,22 @@ function Update-AvdSessionHost {
         else {
             $forceString = "false"
         }
-        $baseParameters = @{
-            hostpoolName      = $HostpoolName
-            resourceGroupName = $ResourceGroupName
-        }
     }
     Process {
-        switch ($PsCmdlet.ParameterSetName) {
+        switch -Wildcard ($PsCmdlet.ParameterSetName) {
             All {
-                $sessionHosts = Get-AvdSessionHost @baseParameters
+                $sessionHosts = Get-AvdSessionHost -HostpoolName $HostpoolName -ResourceGroupName $ResourceGroupName
             }
-            Hostname {
-                $sessionHosts = Get-AvdSessionHost @baseParameters -Name $SessionHostName
+            *Hostname {
+                $sessionHosts = Get-AvdSessionHost -HostpoolName $HostpoolName -ResourceGroupName $ResourceGroupName -Name $SessionHostName
             }
-            default {
+            Id* {
                 $sessionHosts = Get-AvdSessionHost -Id $Id
             }
         }
         $sessionHosts | ForEach-Object {
             try {
                 Write-Verbose "Updating sessionhost $vmName"
-                
                 $url = "{0}{1}?api-version={2}&force={3}" -f $Script:AzureApiUrl, $_.id , $apiVersion, $forceString
                 Write-Verbose $url
                 $parameters = @{
@@ -99,11 +105,14 @@ function Update-AvdSessionHost {
                     properties = @{
                         AllowNewSession = $AllowNewSession
                         FriendlyName    = $FriendlyName
-                        AssignedUser    = $AssignedUser
                     }
                 }
                 if ($AssignedUser){
                     $body.properties.add('AssignedUser',$AssignedUser)
+                }
+                if ($UnAssignAll.IsPresent) {
+                    Write-Verbose "Unassigning all users from sessionhost"
+                    $body.properties.add('AssignedUser', "")
                 }
                 $parameters = @{
                     URI     = $url 
