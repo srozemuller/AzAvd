02270b6e953075e0097d48cff3b2f445797f3f00
srozemuller <s.rozemuller@exite.com>
2023-05-06 21:53:10 +0200

    option to add,remove,export avd workbooks

---
+++ Az.Avd/Private/Request-Api.ps1
@@ -0,0 +1,57 @@
+function Request-Api {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Method = "GET",
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Uri,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [object]$Headers,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [object]$Body
+    )
+    Begin {
+        Write-Verbose "Requesting url $Uri with method $Method"
+        $resultObject = [System.Collections.ArrayList]::new()
+    }
+    Process {
+        try {
+            $parameters = @{
+                Uri     = $Uri
+                Method  = $Method
+                Headers = $Headers
+            }
+            if ($Body) {
+                $parameters.Add("Body", $Body) > $null
+            }
+            $results = (Invoke-WebRequest @parameters) | ConvertFrom-Json
+            $resultObject = $results.value
+            if ($null -eq $results.value) {
+                $resultObject = $results
+            }
+            while (($null -ne $results."@odata.nextLink")) {
+                $pagingUrl = $results."@odata.nextLink"
+                Write-Verbose "Fetching odata.nextLink: $pagingUrl"
+
+                $results = (Invoke-WebRequest -Uri $pagingUrl -Method $Method -Headers $Headers) | ConvertFrom-Json
+                foreach ($value in $results.content.value) {
+                    $resultObject.Add($value) > $null
+                }
+            }
+
+        }
+        catch [System.Exception] {
+            Write-Error -Message "An error occurred while requesting url $uri. Error message: $($PSItem.Exception.Message)"
+        }
+    }
+    End {
+        return $resultObject
+    }
+}
---
+++ Az.Avd/Public/Export-AvdWorkbook.ps1
@@ -0,0 +1,63 @@
+function Export-AvdWorkbook {
+    <#
+    .SYNOPSIS
+    Exports the provided workbook
+    .DESCRIPTION
+    Exports the workbook to a JSON formatted file on the provided file location
+    .PARAMETER WorkbookName
+    Enter the workbook name(s) to export
+    .PARAMETER Id
+    Enter the workbook resource ID
+    .PARAMETER ExportPath
+    The path to export the JSON files to
+    .EXAMPLE
+    Get-AvdWorkbook -WorkbookName "Workbook 1" | Export-AvdWorkbook
+    .EXAMPLE
+    Export-AvdWorkbook -WorkbookName @("Workbook 1", "Workbook") -ExportPath .\
+    #>
+    [CmdletBinding(DefaultParameterSetName = "Name")]
+    param (
+        [Parameter(ParameterSetName = "Name")]
+        [ValidateNotNullOrEmpty()]
+        [array]$WorkbookName,
+
+        [Parameter(ParameterSetName = "ResourceId", ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Id,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$ExportPath = ".\"
+    )
+    Begin {
+        Write-Verbose "Start requesting workbooks in $script:subscriptionId for export"
+        AuthenticationCheck
+    }
+    Process {
+        try {
+            switch ($PsCmdlet.ParameterSetName) {
+                Name {
+                    $workbooks = Get-AvdWorkbook -WorkbookName $WorkbookName -ShowContent
+                }
+                ResourceId {
+                    Write-Verbose "ID is $Id"
+                    $workbooks = Get-AvdWorkbook -Id $Id -ShowContent
+                }
+            }
+            $workbooks | ForEach-Object {
+                try {
+                    $filePath = "{0}\{1}.json" -f $ExportPath, ($_.Properties.displayName).Replace(" ", "_")
+                    Write-Verbose "Exporting workbook to $filePath"
+                    $_.Properties.serializedData | Out-File -FilePath $filePath
+                }
+                catch [System.Exception] {
+                    Write-Error -Message "An error occurred while constructing parameter input for access token retrieval. Error message: $($PSItem.Exception.Message)"
+                }
+            }
+        }
+        catch [System.Exception] {
+            Write-Error -Message "An error occurred while constructing parameter input for access token retrieval. Error message: $($PSItem.Exception.Message)"
+        }
+    }
+}
+
