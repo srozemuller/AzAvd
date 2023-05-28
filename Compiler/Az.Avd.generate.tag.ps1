de9be155d172776c40256a7445c8d7681900f093
srozemuller <s.rozemuller@exite.com>
2023-05-28 11:55:25 +0200

    feat: generate tag based on module version

---
+++ Compiler/Az.Avd.generate.tag.ps1
@@ -0,0 +1,22 @@
+[CmdletBinding()]
+param (
+
+)
+try {
+    $env:ProjectName = "Az.Avd"
+    Write-Host "Creating GitHub tag" -ForegroundColor Green
+    $modulePath = "./$env:ProjectName/$env:ProjectName.psd1"
+    $manifest = Import-PowerShellDataFile -Path $modulePath
+    Import-Module $modulePath -Force
+
+    $version = "{0}" -f $manifest.ModuleVersion
+    if ($manifest.PrivateData.PSData.Prerelease) {
+        $version = "{0}-{1}" -f $manifest.ModuleVersion, $manifest.PrivateData.PSData.Prerelease
+    }
+    Write-Information "Version is $version" -InformationAction Continue
+    Write-Output "version=$($version)" >> $Env:GITHUB_OUTPUT
+
+}
+catch {
+    Throw "Not able to create a release, $_"
+}
