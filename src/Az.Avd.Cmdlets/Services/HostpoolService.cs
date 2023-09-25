00cf16c923b097de2f3fc409930968466ddc7f65
Tim Fisicaro <t.fisicaro@xenial.cloud>
2023-09-25 11:21:10 +0200

    refactor: return the object you want to use

--- src/Az.Avd.Cmdlets/Services/HostpoolService.cs
+++ src/Az.Avd.Cmdlets/Services/HostpoolService.cs
@@ -1,10 +1,7 @@
-using System.Globalization;
-using Az.Avd.Cmdlets.Commands.Public;
 using Az.Avd.Cmdlets.Models;
 using Az.Avd.Core.Constants;
 using Az.Avd.Core.Helpers;
-using Azure.ResourceManager.DesktopVirtualization;
-using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace Az.Avd.Cmdlets.Services;
 
@@ -13,43 +10,43 @@ public interface IHostpoolService
     Task<List<Hostpool>?> GetBySubscription(Guid subscriptionId);
 }
 
-
 public sealed class HostpoolService : IHostpoolService
 {
+    private readonly HttpClient _http = new();
+
     public async Task<List<Hostpool>?> GetBySubscription(Guid subscriptionId)
     {
-        
         var url = $"{ApiUrls.AzureApiUrl}/subscriptions/{subscriptionId}/providers/Microsoft.DesktopVirtualization/hostpools?api-version={ApiVersions.HostpoolApiVersion}";
-        var httpClient = new HttpClient();
         var token = MsalHelper.GetTokenFromInteractiveFlow().AccessToken;
-        httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-        {
-            try
-            {
-                // Make a GET request to a specific endpoint
-                Console.WriteLine($"Sending request to: {url}");
-                HttpResponseMessage response = await httpClient.GetAsync(url);
 
-                // Check if the request was successful
-                if (response.IsSuccessStatusCode)
-                {
-                    // Read the response content as a string
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    var goodBody = JsonConvert.DeserializeObject<List<Hostpool>?>(responseBody);
-                    Console.WriteLine(response);
-                    Console.Write(goodBody);
-                }
-                else
-                {
-                    Console.WriteLine($"HTTP Request Error: {response.StatusCode} - {response.ReasonPhrase}");
-                    
-                }
-            }
-            catch (HttpRequestException e)
+        _http.DefaultRequestHeaders.Clear();
+        _http.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+        try
+        {
+            // Make a GET request to a specific endpoint
+            Console.WriteLine($"Sending request to: {url}");
+            var response = await _http.GetAsync(url);
+
+            // Check if the request was successful
+            if (!response.IsSuccessStatusCode)
             {
-                Console.WriteLine($"HTTP Request Exception: {e.Message}");
+                Console.WriteLine($"HTTP Request Error: {response.StatusCode} - {response.ReasonPhrase}");
             }
+
+            // Read the response content as a string
+            var responseStream = await response.Content.ReadAsStreamAsync();
+            var result = await JsonSerializer.DeserializeAsync<List<Hostpool>?>(responseStream);
+
+            Console.Write(result);
+
+            return result; // <-- This is the result we want to return
         }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"HTTP Request Exception: {e.Message}");
+        }
+
         return null;
     }
 }
