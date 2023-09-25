d50eefbe33cc14dce099dc9c4a79865e94c3d5e5
Tim Fisicaro <t.fisicaro@xenial.cloud>
2023-09-25 11:44:35 +0200

    refactor: add json attributes on domain model

--- src/Az.Avd.Cmdlets/Models/Hostpool.cs
+++ src/Az.Avd.Cmdlets/Models/Hostpool.cs
@@ -1,10 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace Az.Avd.Cmdlets.Models;
 
-
-public class Hostpool
+public sealed record Hostpool
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string Id { get; set; }
+    [JsonPropertyName("id")]
+    public string Id { get; init; } = string.Empty;
 
+    [JsonPropertyName("name ")]
+    public string Name { get; init; } = string.Empty;
+
+    [JsonPropertyName("description")]
+    public string Description { get; init; } = string.Empty;
 }
